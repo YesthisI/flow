8cfd560419b315217d3226e465f8d59d36c4c92f
YesthisI <145901443+YesthisI@users.noreply.github.com>
2023-09-25 01:43:00 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='flow',
+      packages=['flow'],
+      version='0.1',
+      description='This module for creating and working with individual threads in the local memory of each.',
+      author = 'Alexander Borovikov',
+      author_email='teiwaz-h@mail.ru',
+      url = 'https://github.com/YesthisI/Flow',
+      keywords = ['stack','local','threaded'],
+      classifiers = []
